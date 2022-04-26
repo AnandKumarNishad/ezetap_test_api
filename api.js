9de1c96e9a7d9c9f07577c6a386fce27d00f2dd4
anand <anand.nishad.19@mountblue.tech>
2022-04-26 15:58:03 +0530

    added agreement text update api

--- api.js
+++ api.js
@@ -107,7 +107,7 @@ app.put("agreements/:agreement_id", (req, res) => {
   const agreement_id = req.params.agreement_id
   const { agreementText } = req.body
   client.query(
-    `UPDATE agreements SET agreementText = ${agreementText} WHERE id = ${agreement_id}`,
+    `UPDATE agreements SET agreementtext = ${agreementText} WHERE id = ${agreement_id}`,
     (err, result) => {
       if(err){
         console.log(err)
